e24bf3d09e18b4fe32899837e8a8919b9d4da1eb
coop152 <kylecoop152@gmail.com>
2026-02-02 20:59:24 +0000

    removed big ass build

--- race_phase.gd
+++ race_phase.gd
@@ -1,5 +1,5 @@
 extends Node3D
 
-func _physics_process(delta: float) -> void:	
+func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("devkey"):
 		$Player.position = Vector3(39, 208, 53)
